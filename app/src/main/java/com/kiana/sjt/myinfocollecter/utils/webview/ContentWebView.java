9a41851816f3dcdd744bd83e4d223f1910261ac2
史健廷 <shijianting3955@163.com>
2018-06-12 14:59:34 +0800

    内容优化

--- app/src/main/java/com/kiana/sjt/myinfocollecter/utils/webview/ContentWebView.java
+++ app/src/main/java/com/kiana/sjt/myinfocollecter/utils/webview/ContentWebView.java
@@ -1,30 +1,31 @@
 package com.kiana.sjt.myinfocollecter.utils.webview;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.media.Image;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.KeyEvent;
 import android.view.View;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.kiana.sjt.myinfocollecter.MainActivity;
 import com.kiana.sjt.myinfocollecter.R;
+import com.tencent.smtt.sdk.CookieSyncManager;
+import com.tencent.smtt.sdk.DownloadListener;
+import com.tencent.smtt.sdk.WebSettings;
+import com.tencent.smtt.sdk.WebView;
+import com.tencent.smtt.sdk.WebViewClient;
 
-import im.delight.android.webview.AdvancedWebView;
 
 /**
  * 展示网页
  * Created by taodi on 2018/4/25.
  */
 
-public class ContentWebView extends MainActivity implements AdvancedWebView.Listener{
+public class ContentWebView extends MainActivity{
 
     public final static String PARAM_URL = "param_url";
     public final static String PARAM_TITLE = "param_title";
@@ -35,7 +36,7 @@ public class ContentWebView extends MainActivity implements AdvancedWebView.List
 
     private ImageView backIv,frontIv,refreshIv;
 
-    private AdvancedWebView webView;
+    private WebView webView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -61,51 +62,81 @@ public class ContentWebView extends MainActivity implements AdvancedWebView.List
         frontIv.setOnClickListener(onClickListener);
         refreshIv = (ImageView) findViewById(R.id.iv_refresh);
         refreshIv.setOnClickListener(onClickListener);
-        webView = (AdvancedWebView) findViewById(R.id.webview);
+        webView = (WebView) findViewById(R.id.webview);
         initWebView(webView);
-        webView.setListener(this, this);
+//        webView.setListener(this, this);
         webView.loadUrl(url);
+        CookieSyncManager.createInstance(this);
+        CookieSyncManager.getInstance().sync();
     }
 
-    private void initWebView(AdvancedWebView webView) {
-        webView.setWebChromeClient(new WebChromeClient());
+    private void initWebView(WebView webView) {
+//        webView.setWebChromeClient(new WebChromeClient());
         webView.getSettings().setJavaScriptEnabled(true);
-        // 自适应屏幕
-        webView.getSettings().setDomStorageEnabled(true);
-        webView.getSettings().setAllowContentAccess(true);
-        webView.getSettings().setDatabaseEnabled(true);
-        webView.getSettings().setSavePassword(false);
-        webView.getSettings().setSaveFormData(false);
-        webView.getSettings().setUseWideViewPort(true);
-        webView.getSettings().setSupportZoom(true);//支持缩放
-        webView.setHorizontalScrollBarEnabled(false);// 水平不显示
-        webView.setVerticalScrollBarEnabled(false); // 垂直不显示
-        webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
-        webView.getSettings().setLoadWithOverviewMode(true);
-        // webview漏洞,android3.0~android4.2移除searchBoxJavaBridge
-        if (Build.VERSION.SDK_INT > 10 && Build.VERSION.SDK_INT <= 17) {
-            webView.removeJavascriptInterface("searchBoxJavaBridge_");
-        }
-        webView.removeJavascriptInterface("accessibility");
-        webView.removeJavascriptInterface("accessibilityTraversal");
+        initDownload(webView);
+        initWebViewClient(webView);
+        WebSettings webSetting = webView.getSettings();
+        webSetting.setAllowFileAccess(true);
+        webSetting.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        webSetting.setSupportZoom(true);
+        webSetting.setBuiltInZoomControls(false);
+        webSetting.setUseWideViewPort(true);
+        webSetting.setSupportMultipleWindows(false);
+        // webSetting.setLoadWithOverviewMode(true);
+        webSetting.setAppCacheEnabled(true);
+        // webSetting.setDatabaseEnabled(true);
+        webSetting.setDomStorageEnabled(true);
+        webSetting.setJavaScriptEnabled(true);
+        webSetting.setGeolocationEnabled(true);
+        webSetting.setAppCacheMaxSize(Long.MAX_VALUE);
+        webSetting.setAppCachePath(this.getDir("appcache", 0).getPath());
+        webSetting.setDatabasePath(this.getDir("databases", 0).getPath());
+        webSetting.setGeolocationDatabasePath(this.getDir("geolocation", 0)
+                .getPath());
+        // webSetting.setPageCacheCapacity(IX5WebSettings.DEFAULT_CACHE_CAPACITY);
+        webSetting.setPluginState(WebSettings.PluginState.ON_DEMAND);
+        // webSetting.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        // webSetting.setPreFectch(true);
+
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        webView.onResume();
+    /**
+     * 设置下载
+     * @param webView
+     */
+    private void initDownload(WebView webView) {
+        webView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String s, String s1, String s2, String s3, long l) {
+
+            }
+        });
     }
 
-    @Override
-    protected void onPause() {
-        webView.onPause();
-        super.onPause();
-    }
+    private void initWebViewClient(WebView webView) {
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView webView, String s) {
+                if (s.startsWith("thunder://")) {
+                    //获取剪贴板管理器：
+                    ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                    // 创建普通字符型ClipData
+                    ClipData mClipData = ClipData.newPlainText("Label", s);
+                    // 将ClipData内容放到系统剪贴板里。
+                    cm.setPrimaryClip(mClipData);
+                    tip("内容已复制到剪切板");
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
 
-    @Override
-    protected void onDestroy() {
-        webView.onDestroy();
-        super.onDestroy();
+            @Override
+            public void onPageFinished(WebView webView, String s) {
+                super.onPageFinished(webView, s);
+            }
+        });
     }
 
     @Override
@@ -137,35 +168,6 @@ public class ContentWebView extends MainActivity implements AdvancedWebView.List
             }
         }
     };
-
-    @Override
-    public void onPageStarted(String url, Bitmap favicon) {
-
-    }
-
-    @Override
-    public void onPageFinished(String url) {
-
-    }
-
-    @Override
-    public void onPageError(int errorCode, String description, String failingUrl) {
-
-    }
-
-    @Override
-    public void onDownloadRequested(String url, String suggestedFilename, String mimeType, long contentLength, String contentDisposition, String userAgent) {
-        //调起外部浏览器进行下载
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.addCategory(Intent.CATEGORY_BROWSABLE);
-        intent.setData(Uri.parse(url));
-        startActivity(intent);
-    }
-
-    @Override
-    public void onExternalPageRequest(String url) {
-
-    }
 
 
 }
